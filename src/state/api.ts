6a85560095bbde95e56f530db62aaafc2d2f9ba0
Shadaab Ahmad <shadaabali200@gmail.com>
2024-08-23 22:54:33 +0530

    first commit

--- src/state/api.ts
+++ src/state/api.ts
@@ -6,7 +6,7 @@ import {
 } from "./types";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://financer-backend-shad.onrender.com/" }),
   reducerPath: "main",
   tagTypes: ["Kpis", "Products", "Transactions"],
   endpoints: (build) => ({
